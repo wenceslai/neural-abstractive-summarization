dbefd9c06d5f9125ca07b6f5a11276e2457cee5b
Vasulda <vaclavkubon2@gmail.com>
2021-01-04 17:14:45 +0100

    backup

--- helper_funcs.py
+++ helper_funcs.py
@@ -1,13 +1,16 @@
-def print_status_bar(epoch, batch_i, total_batches, loss):
+import time
+
+def print_status_bar(epoch, stage, batch_i, total_batches, loss, t):
     
     bar_len = 24
     width = total_batches // bar_len
+    batch_i += 1
     percentage = batch_i / total_batches * 100
 
     progress_done = ">" * (batch_i // width)
     progress_to_go = "." * (bar_len - (batch_i // width))
-    print(f"\repoch: {epoch+1}\t{batch_i:03d}/{total_batches}\t [{progress_done}>{progress_to_go}] ({percentage:.1f}%)\tloss: {loss:.5f} ", end="")
-
-
-
-
+    
+    print(f"\repoch: {epoch+1} \
+        stage: {stage}\
+        batch: {batch_i:03d}/{total_batches} \
+        [{progress_done}>{progress_to_go}] ({percentage:.1f}%)\tloss: {loss:.5f}\tt:{(time.time() - t) // 60}min {(time.time() - t) % 60:.0f}s", end="")
--- model.py
+++ model.py
@@ -1,9 +1,5 @@
 import tensorflow as tf
 #from tensorflow.keras.layers import Dense#, Input, LSTM, Bidirectional, Embedding, Concatenate, RepeatVector, Activation, Dot
-#from keras.models import Model
-#import keras
-#from tensorflow.keras.backend as K
-
 import time
 from helper_funcs import print_status_bar
 
@@ -17,13 +13,13 @@ class Encoder(tf.keras.layers.Layer):
     def call(self, X):
         X = self.embedding(X)
         a = self.bilstm(X)
-        #
+ 
         return a 
 
 
-class AttentionModule(tf.keras.layers.Layer):
+class BahdanauAttention(tf.keras.layers.Layer):
     def __init__(self, units, Tx):
-        super(AttentionModule, self).__init__()
+        super(BahdanauAttention, self).__init__()
         self.dense_linear1 = tf.keras.layers.Dense(units) #Dense is defaultly set to linear activation (None activ)
         self.dense_linear2 = tf.keras.layers.Dense(units)
         self.dense_energy = tf.keras.layers.Dense(1, activation='relu')
@@ -40,9 +36,6 @@ class AttentionModule(tf.keras.layers.Layer):
         e = self.dense_energy(e)
         
         alpha_weights = tf.nn.softmax(e, axis=-1) #scalars!
-    
-        #alpha_weights = tf.transpose(alpha_weights, perm=[0, 2, 1])
-        #context = tf.tensordot(alpha_weights, a, axes=[1, 2])
         
         context = alpha_weights * a
         context = tf.reduce_sum(context, axis=1)
@@ -51,12 +44,26 @@ class AttentionModule(tf.keras.layers.Layer):
         return context
 
 
+class PointerGenerator(tf.keras.layers.Layer):
+    def __init__(self, context_units, h_units, decoder_input_units):
+        super(PointerGenerator, self).__init__()
+        self.Wc = tf.Variable(tf.zeros([1, context_units]), tf.float32) ###########################################
+        self.Wh = tf.Variable(tf.zeros([1, h_units]), tf.float32)
+        self.Wdi = tf.Variable(tf.zeros([1, decoder_input_units]), tf.float32)
+        self.b = tf.Variable()
+    def call(self, context, h, decoder_input):
+        
+        p_gen = tf.math.sigmoid(self.Wc @ context + self.Wh @ h + self.Wdi @ decoder_input + b)
+
+        return p_gen #soft switch for wheter to extract or abstract word
+
+
 class Decoder(tf.keras.layers.Layer):
     def __init__(self, units, vocab_size, embedding_dim, Tx):
         super(Decoder, self).__init__()
         self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-        self.attention_module = AttentionModule(10, Tx) #number of units !!!!!!!
+        self.attention_module = BahdanauAttention(10, Tx) #number of units !!!!!!!
         self.lstm = tf.keras.layers.LSTM(units=units, return_state=True)
         self.dense_softmax = tf.keras.layers.Dense(vocab_size, activation='softmax')
 
@@ -64,7 +71,6 @@ class Decoder(tf.keras.layers.Layer):
         X = self.embedding(X)
 
         context = self.attention_module(a, h)
-
         #print("context ", context.shape, X.shape)
         X = tf.concat([context, X], axis=-1)
 
@@ -88,9 +94,9 @@ class TextSummarizer:
         self.encoder = Encoder(a_units, vocab_size, embedding_dim)
         self.decoder = Decoder(h_units, vocab_size, embedding_dim, Tx)
 
-        #self.loss_function = tf.keras.losses.CategoricalCrossentropy() # CE = - tf.reduce_sum(y_true * log(y_pred))
-        self.loss_function = tf.keras.losses.SparseCategoricalCrossentropy()
+        self.loss_function = tf.keras.losses.SparseCategoricalCrossentropy() #CE = - tf.reduce_sum(y_true * log(y_pred))
         self.optimizer = tf.keras.optimizers.Adam()
+
 
     @tf.function
     def train_step(self, X, y):
@@ -98,15 +104,11 @@ class TextSummarizer:
         loss = 0
         h = tf.zeros([self.batch_size, self.h_units])
         c = tf.zeros([self.batch_size, self.h_units])
-        
         #y = tf.one_hot(y, self.vocab_size)
-        
         with tf.GradientTape() as tape:
             
             a = self.encoder(X)
             
-            #decoder_input = tf.zeros([batch_size, self.vocab_size]) #first input for decoder
-            #decoder_input = decoder_input[:, 1].assign(1) #token value 1 defined in data_preprocessing
             decoder_input = tf.expand_dims([1] * self.batch_size, 1) #?????
             
             for t in range(self.Ty): #decoder loop
@@ -116,9 +118,6 @@ class TextSummarizer:
                 
                 y_true = y[:, t] 
                 y_true = tf.reshape(y_true, [-1, 1])
-
-                #print("y_pred", y_pred.shape)
-                #print("slice", y_true.shape)
 
                 loss += self.loss_function(y_true, y_pred)
                 
@@ -135,28 +134,113 @@ class TextSummarizer:
         return avg_batch_loss
     
 
-    def fit(self, batches, epochs):
-        total_batches = len(batches)
-        
-        for epoch in range(epochs):
-            epoch_loss = 0
+    def fit(self, epochs, train_batches, val_batches=None):
+        print("starting training...")
+        total_train_batches = len(train_batches); total_val_batches = len(val_batches)
+
+        for epoch in range(epochs): #training loop
+            
             t0 = time.time()
-            for batch_i, (X_batch, y_batch) in enumerate(batches):
-                
-                batch_loss = self.train_step(X_batch, y_batch)
-                epoch_loss += batch_loss
-                
-                print_status_bar(epoch, batch_i, total_batches, batch_loss)
+            train_epoch_loss = 0; val_epoch_loss = 0
 
-            print(f"epoch: {epoch} done\t avg_loss: {epoch_loss}\texec_time: {time.time() - t0}")
+            for batch_i, (X_batch, y_batch) in enumerate(train_batches):
+                
+                loss = self.train_step(X_batch, y_batch)
+                train_epoch_loss += loss
+                
+                print_status_bar(epoch, "tra", batch_i, total_train_batches, loss, t0)
+                
+            if val_batches is not None: #validation
+                print("\nvalidating...")
+                val_epoch_loss = self.evaluate(val_batches, self.batch_size)
+            
+            print(f"\repoch: {epoch+1} done\
+                \tavg_train_loss: {train_epoch_loss / total_train_batches}\
+                \tavg_val_loss: {val_epoch_loss / total_val_batches}\
+                \texec_time: {(time.time() - t0)/60:.2f}min")
     
-    #def infer():
 
-    #def plot_attention():
+    @tf.function
+    def evaluate(self, batches, batch_size): #inference time, varible batch_size for few preds
+        t0 = time.time()
+        val_epoch_loss = 0
+        for batch_i, (X, y) in enumerate(batches):
+        
+            loss = 0
+            h = tf.zeros([batch_size, self.h_units])
+            c = tf.zeros([batch_size, self.h_units])
+            
+            decoder_input = tf.expand_dims([1] * batch_size, axis=1) #creating <sos> token
+            
+            a = self.encoder(X)
+            
+            for t in range(self.Ty): #decoder loop
+                
+                y_pred, h, c = self.decoder(decoder_input, a, h, c) #return alphas for attention plotting???
+                
+                y_true = y[:, t] 
+                y_true = tf.reshape(y_true, [-1, 1])
 
-    #def beam_infer():
+                loss += self.loss_function(y_true, y_pred)
+                
+                y_pred = tf.expand_dims(tf.argmax(y_pred, axis=1), axis=1) #decoding one hot vectors to indexes
+                decoder_input = y_pred #passing last prediction
+            
+            avg_batch_loss = loss / batch_size
+            val_epoch_loss += avg_batch_loss
+
+            #print_status_bar(0, "val", batch_i, len(batches), avg_batch_loss, t0)
+
+        return val_epoch_loss
+
+
+    def beam_search(self):
+        pass
+
+
+    def plot_attention(self):
+        pass
 
     #print_stats():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
 """
